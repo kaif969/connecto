a1893678c75e83f322c0a86df33ca07626c1c911
Sk Kaif Uddin <kaifsk969@gmail.com>
2025-08-06 21:56:15 +0530

    Revert "change in axios file"

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
+<<<<<<< HEAD
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/',
+=======
+    baseURL: 'http://localhost:5000/api/',
+>>>>>>> parent of d0f34ad2 (change in axios file)
     withCredentials: true,
     headers: {
     'Content-Type': 'application/json',
